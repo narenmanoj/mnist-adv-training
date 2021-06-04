d26db6a8a55e1b4a6a10be92a11dadc8253e908e
Naren Manoj <manoj.narens@gmail.com>
2021-06-04 10:40:42 -0400

    autopep8

--- adversarial_ml/util.py
+++ adversarial_ml/util.py
@@ -1,6 +1,9 @@
 import tensorflow as tf
 
+
 def convert_to_tfds(examples, labels, batch_size=32, buffer_size=100):
-  if buffer_size == 0:
-    tf.data.Dataset.from_tensor_slices((examples, labels)).batch(batch_size)
-  return tf.data.Dataset.from_tensor_slices((examples, labels)).shuffle(buffer_size).batch(batch_size)
+    if buffer_size == 0:
+        tf.data.Dataset.from_tensor_slices(
+            (examples, labels)).batch(batch_size)
+    return tf.data.Dataset.from_tensor_slices(
+        (examples, labels)).shuffle(buffer_size).batch(batch_size)
